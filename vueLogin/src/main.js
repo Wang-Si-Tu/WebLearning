7ecee95a01ab9785b51130158c21118e17ab5849
WangSiTu <24602250@qq.com>
2020-04-24 21:55:58 +0800

    CSS静态资源更新

--- vueLogin/src/main.js
+++ vueLogin/src/main.js
@@ -1,14 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+Vue.use(ElementUI)
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
 Vue.prototype.$axios=axios
 axios.defaults.baseURL='http://localhost:8888/api'
-Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
