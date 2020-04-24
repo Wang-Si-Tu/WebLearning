2153badd7928d619ee876d57446f276fb4b4d2bb
WangSiTu <24602250@qq.com>
2020-04-24 17:57:30 +0800

    delete

--- SpringbootLogin/src/main/java/com/test/Service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.test.Service;
-
-import com.test.Entity.Users;
-import org.springframework.stereotype.Service;
-
-public interface LoginService {
-    public String login(Users user);
-}
